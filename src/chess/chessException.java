c667fbbaadfbdd96c5cdbffba28fec0eb1ababed
paulokiki <vieiralinspaulo@gmail.com>
2019-07-16 13:07:57 -0300

    Movendo peças

--- src/chess/chessException.java
+++ src/chess/chessException.java
@@ -1,8 +1,10 @@
 
 package chess;
 
+import boardgame.BoardException;
 
-public class chessException extends RuntimeException{
+
+public class chessException extends BoardException{
     private static final long serialVersionUID = 1L;
 
     public chessException(String msg) {
